e9e9228cebec7b6b9647cf5d8d892e9a42d5dd5d
Mariia <maria.liuta0206@gmail.com>
2026-05-07 10:17:08 +0300

    Add files via upload

---
+++ less_1/script/js.js
@@ -0,0 +1,86 @@
+// console.log("hello JS");
+// // домівський елемент
+//
+// console.info("hello JS2");
+// // no difference between info and log
+//
+// console.error("hello JS3");
+// // in console, if you have bad
+//
+// console.warn("hello JS4");
+// // in console you will have yellow triangle
+
+
+// creation of constants(змінна)
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = '2';
+// console.log("Змінна: " + MySecondVar);
+// // конкатинація
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MyFirstVar - MySecondVar);
+//
+// // var first, second = 2, "4";
+//
+// var int = 10;
+// var float = 1.3;
+// // type data - number
+//
+// var bool = true;
+// var bool2 = true; //false
+// var str = 'str';
+// var nothing; //невизначене значення
+// nothing = null;
+
+// num = 2;
+// var num;
+// console.log(num);
+
+
+
+// let num = 15;
+// let num = 13;
+// console.log(num);
+// //error
+//
+// const pi = 3.14;
+// pi = 3.1;
+//we cant change meaning
+
+let a = 21, b = 4;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// let a, b;
+// // a = 2+4;
+// // b = 3
+// a = a+1
+// a+= 1
+//
+// a++; //інкримент
+// a--; //декримент
+//
+// ++a;
+// --a;
+
+//
+// let a = 2, b;
+//
+// b = a++
+// console.log(b);
+
+// let a = '2', b = '3';
+// console.log(a + b);
+// console.log((a - 0) + (b - 0));
+// console.log(number(a) + number(b));
+
+let name = "Ivan";
+console.log("Hello " + name + "!");
+let skill = "WEB";
+console.log(`Привіт ${name}! Твій найкращий скіл - це ${skill} ${1+4}`);
